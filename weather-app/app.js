2e813b62198a074e29be0eec84ceaeadf5f6a1c1
Arnaud Bertieaux <arnaud@Arnauds-MacBook-Air.local>
2019-12-07 15:42:25 +0100

    test

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,7 +4,7 @@ const config = require('./config.js')
 
 const getWeather = async () => {
     try {
-        const response = await rp.get(`${config.darksky_url}/37.8267,-122.4233?units=ca`, {json: true})
+        const response = await rp.get(`${config.darksky_url}/37.8267,-122.4233?units=ca`, {json: true});
         console.log(`${response.daily.data[0].summary} It's currently ${response.currently.temperature} degrees out. There is a ${response.currently.precipProbability}% chance of rain.`)
     } catch (error) {
         console.log('Error: ', error.message)
