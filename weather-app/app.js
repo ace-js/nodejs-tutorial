f0c93ead88ef9d22ec55e39e6c02084aeaf28e33
Arnaud Bertieaux <arnaud@Arnauds-MacBook-Air.local>
2019-12-07 18:02:42 +0100

    Added lodash

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const rp = require('request-promise')
+const get = require('lodash.get')
 
 const config = require('./config.js')
 
@@ -6,7 +7,7 @@ const getWeather = async () => {
     try {
         const [latitude, longitude] = await getCoordinates('Charleroi', 'place', 'be')
         const response = await rp.get(`${config.darksky_url}/${longitude},${latitude}?units=ca`, { json: true })
-        console.log(`${response.daily.data[0].summary} It's currently ${response.currently.temperature} degrees out. There is a ${response.currently.precipProbability}% chance of rain.`)
+        console.log(`${get(response, 'daily.data.0.summary', '')} It's currently ${get(response, 'currently.temperature', 'Unkown')} degrees out. There is a ${get(response, 'currently.precipProbability', 'Unknown')}% chance of rain.`)
     } catch (error) {
         console.log('Error: ', error.message)
     }
@@ -19,13 +20,10 @@ const getCoordinates = async (placeName, placeType, country) => {
         if (country) url += `&country=${country}`
 
         const response = await rp(url, { json: true })
-        const result = response.features[0] || {}
-        console.log(`The coordinates of ${placeName} are ${result.geometry.coordinates[1]}, ${result.geometry.coordinates[0]}`)
-        return result.geometry.coordinates
+        return get(response, 'features.0.center', [])
     } catch (error) {
         throw new Error(error.message)
     }
-
 }
 
 getWeather()
