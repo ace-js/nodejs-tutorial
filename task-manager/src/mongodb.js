99919cb1970ab72c3e1c699ab506fd08266f5d36
Arnaud Bertieaux <arnaud@Arnauds-MacBook-Air.local>
2019-12-15 14:34:35 +0100

    end of section 10

--- task-manager/src/mongodb.js
+++ task-manager/src/mongodb.js
@@ -1,18 +1,29 @@
 // CRUD
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+
+const { MongoClient } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect database')
+const runDb = async () => {
+    try {
+        const client = await MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true })
+        const db = client.db(databaseName)
+
+        // find return a cursor not a result like findOne, this example gives as a paging demo
+        const searchCursor = db.collection('users').find({ age: { $gt: 25 } })
+        const result = { skiped: 2 }
+
+        result.total = await searchCursor.count() 
+        result.data = await searchCursor.skip(2).limit(2).toArray()
+        console.log(result); // 
+
+        // Set as completed all tasks with react in its description and completed value to false
+        await db.collection('tasks').updateMany({ description: {$regex : /.*React.*/}, completed: false}, { $set: { completed: true } })
+
+    } catch (error) {
+        console.log('Unable to connect to db');
     }
-    console.log('Mongodb connected')
-    const db = client.db(databaseName)
-    db.collection('users').insertOne({
-        name: 'Arnaud',
-        age: 30
-    })
-})
+}
+
+runDb()
